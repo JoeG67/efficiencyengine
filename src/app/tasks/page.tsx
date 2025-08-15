c8ec30ccf246b9ac7687d21a7a7c01f6214cdb42
JoeG67 <joachimgerard66@gmail.com>
2025-08-15 13:21:52 +0800

    Created task page for task management

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Tasks() {
+  return (
+    <div>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Tasks</h1>
+        <p>This is where your tasks will be listed.</p>
+      </div>
+    </div>
+  );
+}
