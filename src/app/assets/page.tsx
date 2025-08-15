a8f3efef35324113e76237c29e806d9a3bd81878
JoeG67 <joachimgerard66@gmail.com>
2025-08-15 13:21:43 +0800

    Created assets page for asset management

---
+++ src/app/assets/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Assets() {
+  return (
+    <div>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Assets</h1>
+        <p>This is where your assets will be listed.</p>
+      </div>
+    </div>
+  );
+}
