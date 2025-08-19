bb88bd854e6ec4016ca602e82413e2452f40de07
JoeG67 <joachimgerard66@gmail.com>
2025-08-19 17:13:44 +0800

    Updated sidebar stylings and height for better mobile responsiveness

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,24 +4,41 @@ import Link from "next/link";
 export default function Sidebar({ isOpen }: { isOpen: boolean }) {
   return (
     <aside
-      className={`bg-[#313a46] border-r h-screen transition-all duration-300 ease-in-out flex flex-col ${
+      className={`bg-[#313a46] border-r min-h-screen transition-all duration-300 ease-in-out flex flex-col ${
         isOpen ? "w-64" : "w-16"
       }`}
     >
       <div className="flex items-center justify-center p-6">
-                {isOpen && <h2 className="2xl:text-xl xl:text-lg lg:text-base sm:text-sm font-bold text-white font-sans">EFFICIENCY ENGINE</h2>}
+        {isOpen && (
+          <h2 className="2xl:text-xl xl:text-lg lg:text-base sm:text-sm font-bold text-white font-sans">
+            EFFICIENCY ENGINE
+          </h2>
+        )}
 
-        {!isOpen && <h2 className="2xl:text-xl xl:text-lg lg:text-base sm:text-sm text-white font-bold">EE</h2>}
+        {!isOpen && (
+          <h2 className="2xl:text-xl xl:text-lg lg:text-base sm:text-sm text-white font-bold">
+            EE
+          </h2>
+        )}
       </div>
 
       <nav className="flex flex-col gap-2 p-4 text-white font-sans 2xl:text-lg xl:text-base lg:text-sm sm:text-xs">
-        <Link href="/" className="hover:bg-gray-400 p-2 rounded flex items-center gap-2">
+        <Link
+          href="/"
+          className="hover:bg-gray-400 p-2 rounded flex items-center gap-2"
+        >
           <LayoutDashboard size={20} /> {isOpen && "Dashboard"}
         </Link>
-        <Link href="/tasks" className="hover:bg-gray-400 p-2 rounded flex items-center gap-2">
+        <Link
+          href="/tasks"
+          className="hover:bg-gray-400 p-2 rounded flex items-center gap-2"
+        >
           <Check size={20} /> {isOpen && "Tasks"}
         </Link>
-        <Link href="/assets" className="hover:bg-gray-400 p-2 rounded flex items-center gap-2">
+        <Link
+          href="/assets"
+          className="hover:bg-gray-400 p-2 rounded flex items-center gap-2"
+        >
           <FolderClosed size={20} /> {isOpen && "Asset Management"}
         </Link>
       </nav>
