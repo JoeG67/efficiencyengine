e1cd70a46c710908b349d4bdd5172d5462ad23a7
JoeG67 <joachimgerard66@gmail.com>
2025-08-21 16:47:39 +0800

    Updated background color of layout for better contrast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <div className="flex-1 flex flex-col min-h-screen">
           <Header onToggle={() => setSidebarOpen(!sidebarOpen)} />
 
-          <main className="flex-1 p-4">{children}</main>
+          <main className="flex-1 p-4 bg-[#F0F5FA]">{children}</main>
         </div>
       </body>
     </html>
