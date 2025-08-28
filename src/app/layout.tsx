88cddb518fb447ea26a21c6ca6ea3633d25f25b5
JoeG67 <joachimgerard66@gmail.com>
2025-08-28 10:43:01 +0800

    Removed metadata tag from layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
@@ -16,11 +15,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: 'Efficiency Engine',
-  description: 'Efficiency Engine',
-}
 
 export default function RootLayout({
   children,
