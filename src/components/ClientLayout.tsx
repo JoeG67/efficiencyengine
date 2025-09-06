0f43a681371972296a6c48f72c4737dc936d66ac
JoeG67 <joachimgerard66@gmail.com>
2025-09-06 15:36:50 +0800

    Created footer, added it into main layout

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -3,17 +3,23 @@
 import { useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
+import Footer from "./Footer";
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  return (
-    <div className="flex min-h-screen bg-[#F0F5FA]">
-      <Sidebar isOpen={isSidebarOpen} />
-      <div className="flex-1">
-        <Header onToggle={() => setIsSidebarOpen((prev) => !prev)} />
-        <main className="p-4">{children}</main>
-      </div>
+return (
+  <div className="flex min-h-screen bg-[#F0F5FA] overflow-y-hidden">
+    <Sidebar isOpen={isSidebarOpen} />
+    <div className="flex-1 flex flex-col">
+      <Header onToggle={() => setIsSidebarOpen((prev) => !prev)} />
+      <main className="flex-1 p-4 overflow-y-auto">{children}</main>
+      <Footer />
     </div>
-  );
+  </div>
+);
+
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+<footer className="bg-white text-black p-4 flex justify-center items-center w-full">
+  © 2025 Efficiency Engine. All rights reserved.
+</footer>
+
+  );
+}
